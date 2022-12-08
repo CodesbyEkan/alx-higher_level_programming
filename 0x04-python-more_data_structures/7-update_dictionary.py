442fe35ed08fcbff801fb7ae71bc598e7cf2d0ad
CodesbyEkan <ekanvicky@gmail.com>
2022-12-07 16:26:26 -0800

    7-update_dictionary.py file

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return a_dictionary
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
